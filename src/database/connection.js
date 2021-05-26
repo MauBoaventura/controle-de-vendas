16992a0dcad671112ed184c4ca13c7292adee789
Mauboa <mba097@gmail.com>
2021-05-26 00:20:25 -0300

    fix: corrige erro env

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,7 +4,7 @@ const configuration = require('../../knexfile')
 var connection = knex(configuration.development)
 
 //Se a variavel de ambiente do banco da Heroku for ativado 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.CLEARDB_DATABASE_URL) {
     connection = knex(configuration.production)
     console.log("Configuração de produção")
 }
